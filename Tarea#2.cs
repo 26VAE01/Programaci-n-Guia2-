d347104d28ad22d0d63243b84c532b117d480c35
26VAE01 <ajcholonvianka16db@gmail.com>
2026-04-17 19:13:32 -0600

    Add files via upload

---
+++ Tarea#2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        char c;
+
+        Console.Write("Ingrese un caracter: ");
+        c = char.Parse(Console.ReadLine());
+
+        switch (c)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                Console.WriteLine("Es una vocal");
+                break;
+
+            default:
+                if (c >= '0' && c <= '9')
+                {
+                    Console.WriteLine("Es un digito");
+                }
+                else
+                {
+                    Console.WriteLine("No es vocal ni digito");
+                }
+                break;
+        }
+    }
+}
