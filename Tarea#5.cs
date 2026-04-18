32637802ea7112bd98dbb5eec765c66d913a761f
26VAE01 <ajcholonvianka16db@gmail.com>
2026-04-17 19:26:02 -0600

    Add files via upload

---
+++ Tarea#5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Ejercicio5
+{
+    static void Main()
+    {
+        int cantidad, pago;
+        double precioUnit = 650 * 1.12;
+        double total, descuento = 0, totalFinal;
+
+        Console.Write("Cantidad de impresoras: ");
+        cantidad = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Forma de pago:");
+        Console.WriteLine("1. Efectivo (10%)");
+        Console.WriteLine("2. Tarjeta (5%)");
+        Console.WriteLine("3. Vale (15%)");
+        Console.Write("Seleccione: ");
+        pago = int.Parse(Console.ReadLine());
+
+        total = cantidad * precioUnit;
+
+        switch (pago)
+        {
+            case 1:
+                descuento = total * 0.10;
+                break;
+            case 2:
+                descuento = total * 0.05;
+                break;
+            case 3:
+                descuento = total * 0.15;
+                break;
+            default:
+                Console.WriteLine("Opcion invalida");
+                break;
+        }
+
+        totalFinal = total - descuento;
+
+        Console.WriteLine("Total a pagar: Q" + totalFinal);
+    }
+}
